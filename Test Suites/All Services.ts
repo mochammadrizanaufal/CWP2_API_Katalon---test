023ca774d1679beb5c29fd1e58996df9a3f82548
XTR\riza.naufal <riza.naufal@xtremax.com>
2023-02-14 14:55:12 +0700

    rename UAT to STG

---
+++ Test Suites/All Services.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>All Services</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>80b3fa27-442e-4eea-af6a-0c436a8e2069</testSuiteGuid>
+</TestSuiteEntity>
