96a2f3722236df3745573ee309091efb8045c6fa
Mochammad Riza Naufal <riza.naufal@xtremax.com>
2023-02-15 02:32:21 +0000

    Pull request #1: XDPL-718 : add test case, test objects, test artifacts
    Merge in XQA/cwp2_api_katalon from feature/XDPL-718 to master
    
    Squashed commit of the following:
    
    commit 06f6de3a67bfc4c8a97dc656105bf38184d8d6ac
    Author: XTR\riza.naufal <riza.naufal@xtremax.com>
    Date:   Tue Feb 14 17:34:06 2023 +0700
    
        XDPL-718 : add test case, test objects, test artifacts

--- Test Suites/Security Management.ts
+++ Test Suites/Security Management.ts
@@ -11,4 +11,102 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>ab8949e1-9df0-48ec-8500-c92bf933bd5b</testSuiteGuid>
+   <testCaseLink>
+      <guid>9921444a-2780-4223-bb94-603bbeb8afd4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Security Management/Verify Success Code - GetAppVA</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b980f38e-6723-441f-800d-c96f2de93193</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Security Management/Verify Success Code - GetCredit</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f89dda92-252c-48e7-ae6e-ce73e06719dc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Security Management/Verify Success Code - GetLoadTest</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b90142cf-ebf9-4664-9cf9-7aa04a9e366f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Security Management/Verify Success Code - GetPenetrationTest</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>87b51b30-9f20-4522-8c60-65f9465de214</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Security Management/Verify Success Code - GetStaticCodeScan</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5dea7e0b-c178-4f07-8976-9a6cb5241dd8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Security Management/Verify Success Code - ListAppVA</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>76874828-94dd-402d-a5eb-1e561786cc55</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Security Management/Verify Success Code - ListLoadTest</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e8cb370e-9ee4-4571-917e-1c653315013a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Security Management/Verify Success Code - ListLoadTestOption</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b9f81f95-1bbb-40b0-b457-10073f372753</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Security Management/Verify Success Code - ListPenetrationTest</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0d2cbc84-ab15-45fc-b59b-e1581b113ff2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Security Management/Verify Success Code - ListStaticCodeScan</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>87aa4902-01bc-474f-ad4b-d6f0a2ccdd94</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Security Management/Verify Success Code - UploadAppVA</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c72d1d63-3a87-4764-b097-7cd6974fd936</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Security Management/Verify Success Code - UploadLoadTest</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c347132f-4354-4783-a660-6e2569eeba58</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Security Management/Verify Success Code - UploadPenetrationTest</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ef4fa04d-8b2e-4042-869f-61705ed87e00</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Security Management/Verify Success Code - UploadStaticCodeScan</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
